c59d2097fdad2d9a5ad306ead3834cfc356ba3a6
DivinieBanhBao <isaacnguyen123@gmail.com>
2023-11-16 04:49:54 -0600

    Add Shopping Cart

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('logout/',views.logout_user,name='logout'),
     path('register/',views.register_user,name='register'),
     path('product/<int:pk>', views.product, name='product'), #go to product page based on product (primary key) number
-    path('category/<str:cate>',views.category,name='category'), 
+    path('category/<str:cate>',views.category,name='category'),
+    path('add-to-cart/<int:pk>/', views.add_to_cart, name='add_to_cart'),
+    path('view-cart/', views.view_cart, name='view_cart'),
+    path('get-cart-number/', views.get_cart_number, name='get_cart_number'), 
 ]
