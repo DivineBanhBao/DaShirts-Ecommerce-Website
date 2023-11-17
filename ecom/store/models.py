fcb7e1cce0eb21d81275d0dbb51dc789260ab89b
DivinieBanhBao <isaacnguyen123@gmail.com>
2023-11-17 03:14:34 -0600

    Updated Shopping Cart

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.utils import timezone
 #Categories of Products
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -12,10 +14,11 @@ class Category(models.Model):
 
 #Customers
 class Customer(models.Model):
-    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
     first_name = models.CharField(max_length=50) 
     last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=10)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
 
@@ -23,6 +26,7 @@ class Customer(models.Model):
         return f'{self.first_name} {self.last_name}'
 #All Products
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
@@ -34,18 +38,35 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 #Orders
+#class Order(models.Model):
+    #product = models.ForeignKey(Product, on_delete=models.CASCADE)
+   #customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    #quantity = models.IntegerField(default=1)
+    #address = models.CharField(max_length=100, default='', blank=True)
+   #phone = models.CharField(max_length=20, default='', blank=True)
+    #date = models.DateField(default=datetime.datetime.today)
+    #status = models.BooleanField(default=False)
+
+    #def __str__(self):
+        #return self.product
 class Order(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-    address = models.CharField(max_length=100, default='', blank=True)
-    phone = models.CharField(max_length=20, default='', blank=True)
-    date = models.DateField(default=datetime.datetime.today)
-    status = models.BooleanField(default=False)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    products = models.ManyToManyField(Product, through='OrderItem')
+    order_date = models.DateTimeField(default=timezone.now)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.product
+        return f"Order {self.id} by {self.customer.user.username}"
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.id}"
 class Cart(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
